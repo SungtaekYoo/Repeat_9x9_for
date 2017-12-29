09dc62dfb248b3cd71af14e3e24fdadd9179d614
Dennis <Dennis@DESKTOP-V63LC0R>
2017-12-29 16:29:20 +0900

    Initial

---
+++ src/Dennis/Sungtaek/main.java
@@ -0,0 +1,15 @@
+package Dennis.Sungtaek;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<10;i++) {
+			for(int j=1;j<10;j++) {
+				System.out.println(i + "*" + j + "=" + i*j);
+			}
+		}
+	}
+
+}
