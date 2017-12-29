4c560c5370d177c5f7fa4b222d1969f46c24e0dc
Dennis <Dennis@DESKTOP-V63LC0R>
2017-12-29 16:33:16 +0900

    adjust appearance

--- src/Dennis/Sungtaek/main.java
+++ src/Dennis/Sungtaek/main.java
@@ -5,9 +5,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=1;i<10;i++) {
-			for(int j=1;j<10;j++) {
-				System.out.println(i + "*" + j + "=" + i*j);
+		for(int i=1;i<10;i++,System.out.println()) {
+			for(int j=1;j<10;j++,System.out.print("\t")) {
+				System.out.print(i + "*" + j + "=" + i*j);
 			}
 		}
 	}
